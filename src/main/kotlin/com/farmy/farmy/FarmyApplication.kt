288c34f6ede618039c5985118c12cc3c838e044e
andrey <andrewyelmanov@gmail.com>
2025-07-13 11:45:50 +0200

    Init commit

---
+++ src/main/kotlin/com/farmy/farmy/FarmyApplication.kt
@@ -0,0 +1,11 @@
+package com.farmy.farmy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FarmyApplication
+
+fun main(args: Array<String>) {
+    runApplication<FarmyApplication>(*args)
+}
---
+++ src/test/kotlin/com/farmy/farmy/FarmyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.farmy.farmy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FarmyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
